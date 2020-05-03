525e6d270fd9eed3a1d8949a8a1faa2bcbec98d8
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-03 21:09:42 +0200

    Break out search component

---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <>
+        <p>This is the search page</p>
+      </>
+    );
+  }
+}
+
+export default Search;
