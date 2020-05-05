67966dd5d7dae790720fa5db11e809b776b19a55
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-05 14:06:22 +0200

    Implement search handling

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Search.css';
+import Bookshelf from './Bookshelf';
+import * as BooksAPI from './BooksAPI';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
+  state = {
+    results: [],
+  };
+
+  updateSearch = async (event) => {
+    console.log(event.target.value);
+
+    const searchString = event.target.value;
+    if (searchString && searchString.length > 0) {
+      const results = await BooksAPI.search(event.target.value);
+      if (results) {
+        this.setState({ results });
+        console.log(results);
+      }
+    } else {
+      this.setState({ results: [] });
+    }
+  };
+
   render() {
     return (
       <>
@@ -12,16 +34,28 @@ class Search extends Component {
               <button className="close-search">Close</button>
             </Link>
             <div className="search-books-input-wrapper">
-              <input type="text" placeholder="Search by title or author" />
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                onChange={this.updateSearch}
+              />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <Bookshelf
+              title="Search Results"
+              books={this.state.results}
+              updateShelfHandler={this.props.updateShelfHandler}
+            />
           </div>
         </div>
       </>
     );
   }
 }
+
+Bookshelf.propTypes = {
+  updateShelfHandler: PropTypes.func.isRequired,
+};
 
 export default Search;
